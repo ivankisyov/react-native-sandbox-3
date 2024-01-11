8559b8e0ab641fa37b28802857dacd8232e45fba
Ivan Kisyov <dev.vanks@gmail.com>
2024-01-11 23:17:12 +0200

    Add password hashing and user authentication

--- track-server/src/models/User.js
+++ track-server/src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 // Create a schema for the user model
 const userSchema = new mongoose.Schema({
@@ -12,6 +13,49 @@ const userSchema = new mongoose.Schema({
     required: true, // Password is required
   },
 });
+
+userSchema.pre("save", function (next) {
+  // If the password has not been modified, continue
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  // Generate a salt and use it to hash the user's password
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+
+      this.password = hash; // Replace the user provided password with the hash
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function (candidatePassword) {
+  // Create a new promise
+  return new Promise((resolve, reject) => {
+    // Compare the candidate password with the password in the database
+    bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+      if (err) {
+        return reject(err);
+      }
+
+      // If they do not match, reject the promise
+      if (!isMatch) {
+        return reject(false);
+      }
+
+      // Otherwise, resolve the promise
+      resolve(true);
+    });
+  });
+};
 
 // Create a model for the user schema
 mongoose.model("User", userSchema);
--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -18,4 +18,35 @@ router.post("/signup", async (req, res) => {
   }
 });
 
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+
+  // If the user did not provide an email or password, return an error
+  if (!email || !password) {
+    return res
+      .status(422)
+      .send({ error: "Must provide an email and password" });
+  }
+
+  // Find the user by their email address
+  const user = await User.findOne({ email });
+
+  // If the user was not found, return an error
+  if (!user) {
+    return res.status(422).send({ error: "Invalid password or email" });
+  }
+
+  try {
+    // Otherwise, compare the provided password with the password in the database
+    await user.comparePassword(password);
+    // If they match, generate a token
+    const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY");
+    // Send the token to the user
+    res.send({ token });
+  } catch (error) {
+    // If they do not match, return an error
+    return res.status(422).send({ error: "Invalid password or email" });
+  }
+});
+
 module.exports = router;
