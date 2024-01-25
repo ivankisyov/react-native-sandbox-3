952b9817e1a9e009b9dff4e3470290a73f7f01eb
Ivan Kisyov <dev.vanks@gmail.com>
2024-01-25 09:49:33 +0200

    Add AuthForm component and refactor SignupScreen

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,48 +1,23 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import { View, StyleSheet, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import { Text, Input, Button } from "react-native-elements";
+import { Text } from "react-native-elements";
 import Spacer from "../components/Spacer";
 import { Context as AuthContext } from "../context/AuthContext";
+import AuthForm from "../components/AuthForm";
 
 const SignupScreen = () => {
   const { state, signup } = useContext(AuthContext);
   const navigation = useNavigation();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
 
   return (
     <View style={styles.container}>
-      <Spacer>
-        <Text h3>Sign Up for Tracker</Text>
-      </Spacer>
-      <Input
-        label="Email"
-        value={email}
-        onChangeText={setEmail}
-        autoCapitalize="none"
-        autoCorrect={false}
+      <AuthForm
+        headerText="Sign Up for Tracker"
+        errorMessage={state.errorMessage}
+        submitButtonText="Sign Up"
+        onSubmit={signup}
       />
-      <Spacer />
-      <Input
-        label="Password"
-        value={password}
-        onChangeText={setPassword}
-        autoCapitalize="none"
-        autoCorrect={false}
-        secureTextEntry
-      />
-      {state.errorMessage ? (
-        <Text style={{ color: "red" }}>{state.errorMessage}</Text>
-      ) : null}
-      <Spacer>
-        <Button
-          title="Sign Up"
-          onPress={() => {
-            signup({ email, password });
-          }}
-        />
-      </Spacer>
       <TouchableOpacity onPress={() => navigation.navigate("Signin")}>
         <Spacer>
           <Text style={{ color: "blue" }}>
