4c02f8f5455384cc2c0de8e6e9b0bd1b36b8453f
Ivan Kisyov <dev.vanks@gmail.com>
2024-01-26 09:17:28 +0200

    Add location permission and display a polyline on the map

--- tracks/src/components/Map.js
+++ tracks/src/components/Map.js
@@ -1,11 +1,46 @@
 import React from "react";
 import { Text, Input, Button } from "react-native-elements";
 import { View, StyleSheet } from "react-native";
+import MapView, { Polyline } from "react-native-maps";
 
 const Map = () => {
-  return <Text h3>Map</Text>;
+  let mockedPoints = [];
+  for (let i = 0; i < 20; i++) {
+    if (i % 2 === 0) {
+      mockedPoints.push({
+        latitude: 41.5760384 + i * 0.001,
+        longitude: 24.772608 + i * 0.001,
+      });
+    } else {
+      mockedPoints.push({
+        latitude: 41.5760384 - i * 0.002,
+        longitude: 24.772608 + i * 0.001,
+      });
+    }
+  }
+
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude: 41.5760384,
+        longitude: 24.772608,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      }}
+    >
+      <Polyline coordinates={mockedPoints} />
+    </MapView>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  map: {
+    height: 300,
+    borderColor: "black",
+    borderWidth: 1,
+    width: "100%",
+  },
+});
 
 export default Map;
