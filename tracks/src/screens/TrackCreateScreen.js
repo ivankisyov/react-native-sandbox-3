e9e4944c16281c290c32b074b0aeb0a35768cccf
Ivan Kisyov <dev.vanks@gmail.com>
2024-01-28 16:39:33 +0200

    Add mock location for testing and track user's location

--- tracks/src/screens/TrackCreateScreen.js
+++ tracks/src/screens/TrackCreateScreen.js
@@ -2,7 +2,12 @@ import React, { useEffect, useState } from "react";
 import { View, StyleSheet, Button } from "react-native";
 import { Text, Input } from "react-native-elements";
 import Map from "../components/Map";
-import { requestForegroundPermissionsAsync } from "expo-location";
+import {
+  requestForegroundPermissionsAsync,
+  watchPositionAsync,
+  Accuracy,
+} from "expo-location";
+import "../mocks/_mockLocation";
 
 const TrackCreateScreen = () => {
   const [err, setErr] = useState(null);
@@ -13,6 +18,16 @@ const TrackCreateScreen = () => {
       if (!granted) {
         throw new Error("Location permission not granted");
       }
+      await watchPositionAsync(
+        {
+          accuracy: Accuracy.BestForNavigation,
+          timeInterval: 1000,
+          distanceInterval: 10,
+        },
+        (location) => {
+          console.log(location);
+        }
+      );
     } catch (e) {
       setErr(e);
     }
